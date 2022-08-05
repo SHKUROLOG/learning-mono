405ebbabde9127e14b8eb3bea629f72f4c1b89d5
Aleksandr <75541723+SHKUROLOG@users.noreply.github.com>
2022-08-05 04:45:20 +0300

    6 commit

--- packages/learning-site/src/app/router/routes.ts
+++ packages/learning-site/src/app/router/routes.ts
@@ -3,12 +3,10 @@ import CategoriesPage from '../views/CategoriesPage.vue'
 import CategoryPage from '../views/CategoryPage.vue'
 import ThemePage from '../views/ThemePage.vue'
 import StatisticPage from '../views/StatisticPage.vue'
-import AdminPage from '../views/AdminPage.vue'
 
 export const routes: RouteRecordRaw[] = [
   { path: '/', component: CategoriesPage, name: 'home' },
   { path: '/categories/:categoryId', component: CategoryPage, props: true, name: 'category' },
   { path: '/themes/:themeId', component: ThemePage, props: true, name: 'theme' },
   { path: '/statistic', component: StatisticPage, name: 'statistic' },
-  { path: '/admin', component: AdminPage, name: 'admin' },
 ]
