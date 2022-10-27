8a5477dbaea8debe7f935528689ef010ac6838ef
Aleksandr <75541723+SHKUROLOG@users.noreply.github.com>
2022-10-27 18:02:17 +0300

    add animation categoryTile

--- packages/frontend/src/app/components/CategoryTile/CategoryTile.vue
+++ packages/frontend/src/app/components/CategoryTile/CategoryTile.vue
@@ -3,9 +3,11 @@
     <div :class="$style.image"
          :style="{ backgroundImage: `url(${svg})` }"/>
 
-    <router-link v-text="category.title"
-                 :class="$style.link"
-                 :to="{ name: 'category', params: { categoryId: category.id } }"/>
+    <router-link :class="$style.link"
+                 :to="{ name: 'category', params: { categoryId: category.id } }">
+      <div v-text="category.title"
+           :class="$style.title"/>
+    </router-link>
   </div>
 </template>
 
@@ -43,6 +45,17 @@ const svg = computed(() => getSvgUrl(props.category.image))
   scale: 1.1;
 }
 
+.title {
+  position: absolute;
+  text-align: center;
+  width: 100%;
+  opacity: 0;
+  background: rgba(37, 188, 80, 0.8);
+  /* clip-path: polygon(90% 0%, 100% 100%, 0% 100%, 0% 0%); */
+  clip-path: polygon(95% 0%, 100% 100%, 0% 100%, 0% 0%);
+  /* clip-path: polygon(100% 0%, 90% 100%, 0% 100%, 0% 0%); */
+}
+
 .image {
   position: absolute;
   width: 100%;
@@ -52,7 +65,6 @@ const svg = computed(() => getSvgUrl(props.category.image))
   pointer-events: none;
   left: 0;
   top: 0;
-  /* transform: rotate(-15deg) translateY(70%); */
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
@@ -68,12 +80,54 @@ const svg = computed(() => getSvgUrl(props.category.image))
   height: 100%;
   position: relative;
   transition: all 0.3s ease-in-out;
-  padding: 8px;
   box-sizing: border-box;
 }
 
-/* .root:hover .link {
-  border-radius: 2px;
-  background: rgba(0, 0, 0, 0.15);
-} */
+.root:hover .title {
+  color: rgba(0, 0, 0, 0.8);
+  opacity: 0.3;
+  animation: slide-in-blurred-left 0.6s cubic-bezier(0.230, 1.000, 0.320, 1.000) both;
+}
+
+@-webkit-keyframes slide-in-blurred-left {
+  0% {
+    -webkit-transform: translateX(-100px) scaleX(2.5) scaleY(0.2);
+            transform: translateX(-100px) scaleX(2.5) scaleY(0.2);
+    -webkit-transform-origin: 100% 50%;
+            transform-origin: 100% 50%;
+    -webkit-filter: blur(40px);
+            filter: blur(40px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateX(0) scaleY(1) scaleX(1);
+            transform: translateX(0) scaleY(1) scaleX(1);
+    -webkit-transform-origin: 50% 50%;
+            transform-origin: 50% 50%;
+    -webkit-filter: blur(0);
+            filter: blur(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-in-blurred-left {
+  0% {
+    -webkit-transform: translateX(-100px) scaleX(2.5) scaleY(0.2);
+            transform: translateX(-100px) scaleX(2.5) scaleY(0.2);
+    -webkit-transform-origin: 100% 50%;
+            transform-origin: 100% 50%;
+    -webkit-filter: blur(40px);
+            filter: blur(40px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateX(0) scaleY(1) scaleX(1);
+            transform: translateX(0) scaleY(1) scaleX(1);
+    -webkit-transform-origin: 50% 50%;
+            transform-origin: 50% 50%;
+    -webkit-filter: blur(0);
+            filter: blur(0);
+    opacity: 1;
+  }
+}
 </style>
