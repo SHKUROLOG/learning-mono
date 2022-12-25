6555b7a97649bbb566546c54ee8a102af842115c
syntax-dot <alekseew.aleksandr2012@yandex.ru>
2022-12-25 22:18:49 +0300

    wip

--- packages/frontend/src/app/components/AnswerRow/AnswerRow.vue
+++ packages/frontend/src/app/components/AnswerRow/AnswerRow.vue
@@ -17,13 +17,18 @@ defineProps<AnswerRowProps>()
   user-select: none;
   cursor: pointer;
   padding: 8px;
-  color: #25bc50c0;
-  transition: 0.3s ease-in-out;
+  /* color: #25bc50c0; */
+  color: #25bc50;
+  opacity: 0.8;
+  transition: opacity 0.3s;
+  border-left: 2px solid #25bc50c0;
 }
 
 .root:hover {
   transform: scale(1.05) translateX(4px);
-  color: #25bc50;
+  /* color: #25bc50; */
+  opacity: 0.8;
+  border-left: 2px solid #25bc50;
   /* background: #25bc50; */
 }
 </style>
