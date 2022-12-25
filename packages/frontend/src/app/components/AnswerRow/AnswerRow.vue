351f2c16a5fcac2dca68ebd7da98574f9a12fc09
syntax-dot <alekseew.aleksandr2012@yandex.ru>
2022-12-25 22:30:33 +0300

    wip

--- packages/frontend/src/app/components/AnswerRow/AnswerRow.vue
+++ packages/frontend/src/app/components/AnswerRow/AnswerRow.vue
@@ -13,22 +13,21 @@ defineProps<AnswerRowProps>()
 
 <style module>
 .root {
+  display: flex;
   transition: all 0.3s ease-in-out;
   user-select: none;
   cursor: pointer;
   padding: 8px;
-  /* color: #25bc50c0; */
-  color: #25bc50;
-  opacity: 0.8;
-  transition: opacity 0.3s;
+  color: #25bc50c0;
+  transition: 0.3s ease-in-out;
   border-left: 2px solid #25bc50c0;
 }
 
 .root:hover {
   transform: scale(1.05) translateX(4px);
-  /* color: #25bc50; */
-  opacity: 0.8;
-  border-left: 2px solid #25bc50;
+  color: #25bc50;
+  /* opacity: 0.8; */
+  /* border-left: 2px solid #25bc50; */
   /* background: #25bc50; */
 }
 </style>
